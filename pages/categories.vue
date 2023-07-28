879d22bae8b1d0d20fec12e39406d7a9d2b4e9ad
wesleydematos <wesleydematos3@gmail.com>
2023-07-28 09:23:32 -0300

    feat: created form components

--- pages/categories.vue
+++ pages/categories.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
+  <div class="m-4">
     <p>categories</p>
+    <AppFormInput />
+    <AppButton> Adicionar </AppButton>
   </div>
 </template>
 
 <script>
-export default {};
+import AppButton from "~/components/Ui/AppButton";
+import AppFormInput from "~/components/Ui/AppFormInput";
+
+export default {
+  name: "categoriesPage",
+
+  components: {
+    AppButton,
+    AppFormInput,
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
 </script>
 
 <style></style>
