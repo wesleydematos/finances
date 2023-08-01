ea30fda9287bea18624e84cd1f01985e577f9ef0
wesleydematos <wesleydematos3@gmail.com>
2023-08-01 16:15:20 -0300

    feat: created actions

---
+++ store/transactions/actions.js
@@ -0,0 +1,16 @@
+export default {
+  getTransactions(context, filter = {}) {
+    return this.$axios.$get("transactions", {
+      params: {
+        _expand: "category",
+        ...filter,
+      },
+    });
+  },
+  addTransaction(context, data) {
+    return this.$axios.$post("transactions", data);
+  },
+  updateTransaction(context, { id, data }) {
+    return this.$axios.$patch(`transactions/${id}`, data);
+  },
+};
